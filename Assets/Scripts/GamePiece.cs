6042e2077968cff195199a57125149cba67d1cec
Tom-Tomz <142879458+Tom-Tomz@users.noreply.github.com>
2025-02-19 09:11:41 +0100

    Sat kode ind fra program til Unity
    har sat Gamepiece kode ind, og "oversat" det, samt proppet den rå kode ind i Player, det skal rettes næste gang

--- Assets/Scripts/GamePiece.cs
+++ Assets/Scripts/GamePiece.cs
@@ -1,7 +1,19 @@
+using System.Numerics;
 using UnityEngine;
 
 public class GamePiece : MonoBehaviour
 {
+    private int position=0;
 
-    
+    void Start()
+    {
+        UnityEngine.Vector3 pos = transform.position;
+    }
+    public int Position { get => position; }
+
+    public void Move(int x)
+    {
+        position += x;
+        
+    }
 }
